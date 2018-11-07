a3008630290bc3e512b64f65611eac046c707cfb
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    X00TD: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- device.mk
+++ device.mk
@@ -78,6 +78,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
