9a457845c5c475a73f01907233198e27c30accd7
nguyenhung9x <nguyenhung9x2018@gmail.com>
2020-03-18 15:37:01 +0700

    X00TD: Update vendor security patch from ASUS
    require version-firmware=16.2017.2002.061

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,7 +188,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-06-05
+VENDOR_SECURITY_PATCH := 2020-02-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
